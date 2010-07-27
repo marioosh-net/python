d779480dfed329576224dc769e970df9cec6efc4
marioosh <marioosh@15dd19fa-65d8-43a4-bfc9-50ebb766958d>
2010-07-27 11:47:57 +0000

    git-svn-id: svn+ssh://marioosh.net/var/svn/python@3 15dd19fa-65d8-43a4-bfc9-50ebb766958d

--- python_learn/src/net/marioosh/python/learn/simple.py
+++ python_learn/src/net/marioosh/python/learn/simple.py
@@ -4,14 +4,22 @@ Created on 2010-07-27
 @author: muniek
 '''
 
+# operacje matematyczne
 a = 2
 print(a)
 
+
+# stringi
 s = 'ALA ma kota'
 print(len(s))
 print(s)
 print(s.lower())
 
+string1 = 'ala ma kotka" '
+print(string1.strip())
+
+
+# listy
 s = [];
 s.append("sdsds");
 s.append("sdsds");
@@ -19,5 +27,4 @@ s.append("sdsds");s.append("sdsds");
 for i in s: 
     print(i);
 print('kk')
-
 
