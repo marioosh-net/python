c91057375ddcf893f1b51d0dcf357a12a5f5ea93
marioosh <marioosh@15dd19fa-65d8-43a4-bfc9-50ebb766958d>
2010-07-28 08:58:04 +0000

    git-svn-id: svn+ssh://marioosh.net/var/svn/python@6 15dd19fa-65d8-43a4-bfc9-50ebb766958d

--- python_learn/src/net/marioosh/python/learn/exceptions.py
+++ python_learn/src/net/marioosh/python/learn/exceptions.py
@@ -16,12 +16,24 @@ class Exceptions:
     def test(self):
         while self.error == 1:
             try:
-                int(input("INT: "))
+                self.a = int(input("INT: "))
                 self.error = 0
             except:
                 print("podano wartosc NIEliczbowa")
                 self.error = 1
         print("test() end.")
     
+    def plus2(self, a):
+        x = a + 2
+        return x
+    
+    # wyjatek danego typu
+    def test2(self):
+        try:
+            print (1/self.a)
+        except ZeroDivisionError:
+            print ("dzielenie przez 0!")
+    
 e = Exceptions()
 e.test()
+e.test2()
