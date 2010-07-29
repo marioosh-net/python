567b14a0e3481ceb415a2a1521fcd995aac74f95
marioosh <marioosh@15dd19fa-65d8-43a4-bfc9-50ebb766958d>
2010-07-29 20:23:56 +0000

    git-svn-id: svn+ssh://marioosh.net/var/svn/python@14 15dd19fa-65d8-43a4-bfc9-50ebb766958d

--- python_learn/src/net/marioosh/python/learn/_os.py
+++ python_learn/src/net/marioosh/python/learn/_os.py
@@ -6,6 +6,7 @@ Created on 2010-07-29
 
 import os, sys
 
+path = ''
 # czy istnieje sciezka
 if len(sys.argv) > 1:
     path = sys.argv[1]
@@ -13,6 +14,33 @@ else:
     print '''
         syntax: ...
     '''
-    exit()
+    
 #print(path)
 print(os.path.exists("/"))
+
+
+if path == '':
+    path = "."
+print("path: "+path)
+
+def info(path):
+    # czy sciezka jest katalogiem ?
+    if os.path.isdir(path):       
+        # s = os.stat(f)
+        print("DIR :"+path),
+    else:
+        print("FILE:"+path),
+    # rozmiar pliku
+    print(os.stat(path).st_size)
+
+
+# lista plikow w katalogu
+if os.path.isdir(path):
+    list = os.listdir(path)                 
+    for f in list:
+        p = os.path.normpath(path+"/"+f)
+        #info(os.path.join(path,f))
+        info(p)
+else:
+    info(path)
+    
