534915be771fcc76eb70f7387095bdf5b51a5fff
marioosh <marioosh@15dd19fa-65d8-43a4-bfc9-50ebb766958d>
2010-07-29 13:33:12 +0000

    git-svn-id: svn+ssh://marioosh.net/var/svn/python@12 15dd19fa-65d8-43a4-bfc9-50ebb766958d

--- python_learn/src/net/marioosh/python/learn/functions.py
+++ python_learn/src/net/marioosh/python/learn/functions.py
@@ -4,6 +4,7 @@ Created on 2010-07-29
 @author: marioosh
 '''
 
+# standardowa funkcja
 def nazwaFunkcji(param1, param2='dupa'):
     'opis funkcji'
     print(param1),
@@ -13,3 +14,15 @@ nazwaFunkcji('ala')
 nazwaFunkcji('ala',param2='ciao')
 nazwaFunkcji(param2='ala',param1='ciao')
 
+# funkcja z dowolna liczba parametrow
+def multi(*params):
+    for param in params:
+        print(param),
+        
+multi('ala','ola','tomek','wacek')
+
+# funckja anonimowa
+def dajFunkcje(n):
+    return lambda n: n+5
+
+dajFunkcje(10)
