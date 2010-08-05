cce8ede863577d6fe21fee2192baebbf84cb206a
marioosh <marioosh@15dd19fa-65d8-43a4-bfc9-50ebb766958d>
2010-08-05 07:24:14 +0000

    git-svn-id: svn+ssh://marioosh.net/var/svn/python@55 15dd19fa-65d8-43a4-bfc9-50ebb766958d

--- python_learn/src/net/marioosh/python/learn/topListaRMF.py
+++ python_learn/src/net/marioosh/python/learn/topListaRMF.py
@@ -17,15 +17,15 @@ lista = soup.find('td', attrs={'width': '690'})
 # top 3
 l1 = lista.find('table', attrs={'class': 'poplista-toptable'})
 for tr in l1.findAll('tr'):
-    print('<span style="font-weight: bold;">' + tr.contents[2].next.string.strip() + '</span> -'),
-    print(tr.contents[2].next.nextSibling.nextSibling.string.strip() + '<br/>')
+    print(str('<span style="font-weight: bold;">' + tr.contents[2].next.string.strip() + '</span> - ')),
+    print(str(tr.contents[2].next.nextSibling.nextSibling.string.strip() + '<br/>'))
 
 # reszta
 l2 = l1.findNextSibling('table')
 for tr in l2.contents:
     drugiTd = tr.contents[1];
-    print('<span style="font-weight: bold;">' + drugiTd.next.string.strip() + '</span>'),
-    print(drugiTd.next.nextSibling.string.strip() + '<br/>')
+    print(str('<span style="font-weight: bold;">' + drugiTd.next.string.strip() + '</span>')),
+    print(str(drugiTd.next.nextSibling.string.strip() + '<br/>'))
 
     
 
