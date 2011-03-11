7aa413d268f4aae08326e424895c558a6f369f06
marioosh <marioosh@15dd19fa-65d8-43a4-bfc9-50ebb766958d>
2011-03-11 09:18:14 +0000

    checkmail
    git-svn-id: svn+ssh://marioosh.net/var/svn/python@86 15dd19fa-65d8-43a4-bfc9-50ebb766958d

---
+++ python_learn/src/net_marioosh_python/mail/checkmail.py
@@ -0,0 +1,47 @@
+import poplib
+import sys
+from email import parser
+
+p = poplib.POP3_SSL('pop.gmail.com')
+p.user('spam@marioosh.net')
+p.pass_('zbc123')
+
+print p.getwelcome();
+print "MESSAGES: " + str(p.stat()[0]);
+
+list = p.list();
+for msg in list[1]:
+    msg_num = msg.split(' ')[0];
+    top = p.top(msg_num, 0);
+    header = top[1];
+
+    # za pomoca parsera    
+    message = parser.HeaderParser().parsestr("\n".join(header));
+    print message['from'];
+    
+    # recznie
+    if False:
+        for c in header:
+            if c[0:8] == 'Subject:':
+                m_subject = c[9:];
+            if c[0:5] == 'From:':
+                m_from = c[6:] 
+        print m_from
+        if m_from == 'mario@marioosh.net':
+            print c;
+            m = p.retr(msg_num);
+            content = m[1];        
+
+    # tylko pierwszy mail        
+    break;
+
+#Get messages from server:
+# messages = [p.retr(i) for i in range(1, len(p.list()[1]) + 1)]
+# Concat message pieces:
+# messages = ["\n".join(mssg[1]) for mssg in messages]
+#Parse message intom an email object:
+# messages = [parser.Parser().parsestr(mssg) for mssg in messages]
+# for message in messages:
+#     print message['subject']
+
+p.quit()
