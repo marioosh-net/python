4b475dd677adb88404c6c7a880c848da54eaf6cc
marioosh <marioosh@15dd19fa-65d8-43a4-bfc9-50ebb766958d>
2011-03-14 10:13:15 +0000

    podpiecie bazy mantisa, klasa BugMaker
    git-svn-id: svn+ssh://marioosh.net/var/svn/python@101 15dd19fa-65d8-43a4-bfc9-50ebb766958d

--- python_learn/src/net_marioosh_python/mail/checkmail.py
+++ python_learn/src/net_marioosh_python/mail/checkmail.py
@@ -2,7 +2,7 @@ import poplib
 import sys
 from email import parser
 from symbol import except_clause
-# import MySQLdb
+import MySQLdb
 
 class Checker:
     def __init__(self, host, user, password):
@@ -64,9 +64,62 @@ class Checker:
     def disconnect(self):
         self.p.quit()
 
+
+class BugMaker:
+    def __init__(self, host, user, password, db):
+        self.conn = MySQLdb.connect(host, user, password, db);
+
+    def list_texts(self):
+        c = self.conn.cursor();
+        c.execute("SELECT * FROM mantis_bug_text_table");
+        rows = c.fetchall()
+        for r in rows:
+            print r
+
+    def list_bugs(self):
+        c = self.conn.cursor();
+        c.execute("SELECT * FROM mantis_bug_table");
+        rows = c.fetchall()
+        for r in rows:
+            print r
+
+    def add_bug(self, email_from, subject, text, email_to):
+
+        # tabelka texty
+        # mantis_bug_text_table.description = text
+        c = self.conn.cursor();
+        c.execute("INSERT INTO mantis_bug_text_table (description) VALUES('"+text+"')")
+        self.conn.commit()
+
+
+        # tabelka bugi
+        # muntis_bug_table.summary = subject
+        # mantis_bug_table.project_id = self.get_proj_id(email_to)
+        # mantis_bug_table.handler_id = self.get_user_id(email_to)
+        # mantis_bug_table.reporter_id = self.get_user_id(email_from)
+        # mantis_bug_table.bug_text_id = mantis_bug_text_table.id
+
+    def get_proj_id_by_user(self, email):
+        return 11;  # LoogAPPS
+
+    def get_proj_id_by_name(self, name):
+        c = self.conn.cursor();
+        c.execute("select id from mantis_project_table where name = '"+name+"'")
+        return c.fetchone()[0]
+
+    def get_user_id(self, email):
+        c = self.conn.cursor();
+        c.execute("select id from mantis_user_table where email = '"+email+"'")
+        row_id = c.fetchone()[0]
+        return row_id
+
+
 checker = Checker('pop3.o2.pl', 'sp4my', 'zbc123');
 print checker.count_message()
 # checker.check()
 checker.disconnect()
 
+# dostep do mysql
+maker = BugMaker("localhost", "bugtracker2", "AhFeiCh2", "bugtracker2");
+maker.list_bugs()
 
